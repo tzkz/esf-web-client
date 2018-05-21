8c01041aba1fb6efaa8361e1e5b7e0d69ba8f762
timurengineer <tjkz87@gmail.com>
2018-05-21 15:39:09 +0600

    test deploy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello world! test deploy 2
+        Hello world! test deploy zzzz
       </div>
     );
   }
