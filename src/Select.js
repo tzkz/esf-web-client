53ec093ce35e612663379d937c2c38bd7c227256
timurengineer <tjkz87@gmail.com>
2018-06-02 15:42:53 +0600

    add custom styles to Select

--- src/Select.js
+++ src/Select.js
@@ -1,8 +1,52 @@
 import React from 'react';
 import ReactSelect from 'react-select';
 
+const styles = {
+  control: (base) => ({
+    ...base,
+    backgroundColor: 'transparent',
+    border: '0',
+    boxShadow: 'none',
+    minHeight: 'initial',
+    cursor: 'pointer',
+  }),
+  valueContainer: (base) => ({
+    ...base,
+    padding: '0 8px',
+    color: 'inherit',
+  }),
+  singleValue: (base) => ({
+    color: 'inherit',
+  }),
+  option: (base) => ({
+    ...base,
+    cursor: 'pointer',
+  }),
+};
+const components = {
+  IndicatorSeparator: () => '',
+  DropdownIndicator: () => (
+    <span
+      style={{
+        width: 0, 
+        height: 0, 
+        borderLeft: '5px solid transparent',
+        borderRight: '5px solid transparent',
+        
+        borderTop: '5px solid #0194bf',
+      }}
+    />
+  ),
+};
+
 const Select = ({ ...props }) => (
-  <ReactSelect {...props} />
+  <ReactSelect
+    isClearable={false}
+    isSearchable={false}
+    styles={styles}
+    components={components}
+    {...props}
+  />
 );
 
 export default Select;
