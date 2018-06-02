4f05ac54358c9491f706440b3a0edcd9cc76fdf6
timurengineer <tjkz87@gmail.com>
2018-06-02 15:43:13 +0600

    add locale state

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ import Footer from './Footer';
 import BottomCta from './BottomCta';
 
 class App extends Component {
+  state = {
+    locale: { value: 'en', label: 'English' },
+  }
+
+  onLocaleChange = (value) => this.setState({ locale: value });
+
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header
+          localeValue={this.state.locale}
+          onLocaleChange={this.onLocaleChange}
+        />
         <Main />
         <Features />
         <BottomCta />
--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SectionContent from './SectionContent';
 import './Header.css';
 import Select from './Select';
 
 const localeOptions = [
   { value: 'en', label: 'English' },
+  { value: 'kz', label: 'Kazakh' },
   { value: 'ru', label: 'Russian' },
-  { value: 'kz', label: 'Kazakh' }
-]
+];
 
-const Header = () => (
+const Header = ({ localeValue, onLocaleChange }) => (
   <header className="header">
     <SectionContent>
       <div className="header-content">
@@ -17,12 +18,23 @@ const Header = () => (
         <div className="nav-bar">
           <div className="nav-item">Contact Us</div>
           <div className="nav-item">
-            <Select options={localeOptions} />
+            <Select
+              options={localeOptions}
+              value={localeValue}
+              onChange={onLocaleChange}
+            />
           </div>
         </div>
       </div>
     </SectionContent>
   </header>
 );
+
+Header.propTypes = {
+  localeValue: PropTypes.shape({
+    value: PropTypes.string,
+  }),
+  onLocaleChange: PropTypes.func,
+};
 
 export default Header;
