32d33ae8c9f247b3d52799c757df6750a138a92d
timurengineer <tjkz87@gmail.com>
2018-06-09 16:31:26 +0600

    add FileInput to LoginInitial

--- src/Landing/LoginInitial.js
+++ src/Landing/LoginInitial.js
@@ -3,28 +3,17 @@ import { FormattedMessage } from 'react-intl';
 
 import './LoginInitial.css';
 
+import FileInput from '../common/FileInput';
+
 const LoginInitial = () => (
   <div className="login-form">
-    <div className="login-description">Start by browsing your authentication certificate</div>
     <div className="browse-button">
-      <label htmlFor="upload-photo" className="file-input-label">
+      <FileInput>
         <FormattedMessage
           id="LoginInitial.BrowseCertificate"
           defaultMessage="Browse Certificate"
         />
-      </label>
-      <input type="file" name="upload-photo" id="upload-photo" className="file-input" />
-    </div>
-    <div className="certificate-link-container">
-      <a
-        href="http://kgd.gov.kz/sites/default/files/ESF/testovye_ecp_dekabr_2017.zip"
-        className="certificate-link"
-      >
-        <FormattedMessage
-          id="LoginInitial.TestCertificateLink"
-          defaultMessage="Download test certificates"
-        />
-      </a>
+      </FileInput>
     </div>
   </div>
 );
