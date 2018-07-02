4f70cefc8ddbdf967963f8ed03701864f9ca9bca
sh-happens <yerbol_kosbayev@list.ru>
2018-07-02 16:24:05 +0600

    mobile_search_esf

--- src/Search.js
+++ src/Search.js
@@ -4,18 +4,34 @@ import { css } from 'emotion';
 import SectionContent from './common/SectionContent';
 
 const container = {
-  backgroundColor: '#f8f8f8'
+  backgroundColor: '#f8f8f8',
 };
+
+const dataContainer = {
+  display: 'flex',
+  justifyContent: 'space-around',
+}
 
 const input = {
   borderRadius: '5px',
   backgroundColor: '#ffffff',
+  border: '0',
+  height: '40px',
+  minWidth: '0',
 };
+
+const customInput = {
+  margin: '5px',
+  flex: '0 1 50%',
+}
 
 const Search = () => (
   <div className={css(container)}>
     <SectionContent>
-      <input type="text" className={css(input)} placeholder="Date from…" />
+      <div className={css(dataContainer)}>
+        <input type="text" className={css(input, customInput)} placeholder="Date from…" />
+        <input type="text" className={css(input, customInput)} placeholder="Date to…" />
+      </div>
     </SectionContent>
   </div>
 );
