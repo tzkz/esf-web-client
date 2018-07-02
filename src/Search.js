22c49c9c398a75306d9b2be4d058b7aa61cc1a1b
sh-happens <yerbol_kosbayev@list.ru>
2018-07-02 18:20:44 +0600

    Reg number changes

--- src/Search.js
+++ src/Search.js
@@ -4,7 +4,7 @@ import { css } from 'emotion';
 import SectionContent from './common/SectionContent';
 
 const container = {
-  padding: '0px 9px',
+  padding: '15px 9px 0px 9px',
   backgroundColor: '#f8f8f8',
 };
 
@@ -23,8 +23,12 @@ const input = {
 };
 
 const customInput = {
-  margin: '22px 3px 10px',
+  margin: '5px 3px 5px',
   flex: '0 1 50%',
+}
+
+const largeInputTypes = {
+  flexBasis: '100%',
 }
 
 const Search = () => (
@@ -33,6 +37,9 @@ const Search = () => (
       <div className={css(dataContainer)}>
         <input type="text" className={css(input, customInput)} placeholder="  Date from…" />
         <input type="text" className={css(input, customInput)} placeholder="  Date to…" />
+      </div>
+      <div className={css(largeInputTypes)}>
+      <input type="text" className={css(input, customInput, largeInputTypes)} placeholder="  Reg number" />
       </div>
     </SectionContent>
   </div>
