d15fb1900e183b979150c78ab4b8c06ed6aa984a
timurzh <tzhkz7@gmail.com>
2018-07-11 21:28:09 +0600

    update button styles in LoginInitial

--- src/Landing/LoginInitial.js
+++ src/Landing/LoginInitial.js
@@ -7,19 +7,29 @@ import './LoginInitial.css';
 import FileInput from '../common/FileInput';
 import Button from '../common/Button';
 
-const demoButton = {
+const signInButton = {
   backgroundColor: 'transparent',
   border: '2px solid white',
   color: 'white',
   ':hover': {
     backgroundColor: 'transparent',
+    border: '2px solid white',
+  },
+}
+
+const demoButton = {
+  backgroundColor: 'white',
+  border: '2px solid white',
+  color: '#744fc6',
+  ':hover': {
+    backgroundColor: 'white',
   },
 };
 
 const LoginInitial = () => (
   <div className="login-form">
     <div className="browse-button-container">
-      <FileInput>
+      <FileInput className={css(signInButton)}>
         <FormattedMessage
           id="LoginInitial.SignIn"
           defaultMessage="Sign In"
