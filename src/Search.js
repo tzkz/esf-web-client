29d1dede357ed5f63184b2ae1af065c3b7a6a402
Yerbol Kosbayev <yerbol_kosbayev@list.ru>
2018-07-19 14:10:47 +0600

    In/Outbound radio buttons

--- src/Search.js
+++ src/Search.js
@@ -12,6 +12,7 @@ const container = {
 const inputRow = {
   display: 'flex',
   padding: '5px 12px',
+  fontSize: '14px',
 };
 
 const dateInput = {
@@ -30,6 +31,33 @@ const largeInput = {
   flex: '1',
 }
 
+const radioInput = {
+  display: 'none',
+  ':checked + label': {
+    backgroundColor: '#327dd0',
+    height: '34px',
+    display: 'flex',
+    borderRadius: '5px',
+    color: ' #ffffff',
+  } 
+}
+
+const radioTypes = {
+  flex: '1',
+  borderRadius: '5px',
+  backgroundColor: '#ffffff',
+  border: '0',
+  height: '40px',
+  minWidth: '0',
+  fontFamily: 'OpenSans',
+}
+
+const radioTypeId = {
+  paddingTop: '10px',
+  display:'flex',
+  justifyContent: 'center',
+}
+
 const Search = () => (
   <div className={css(container)}>
     <SectionContent>
@@ -40,6 +68,16 @@ const Search = () => (
       <div className={css(inputRow)}>
         <TextInput className={css(largeInput)} placeholder="Reg number" />
       </div> 
+      <div className={css(inputRow)}>
+        <div className={css(radioTypes)}>
+          <input type="radio" name="choose1" id="inbound" className={css(radioInput)}/>
+          <label for="inbound" className={css(radioTypeId)}>Inbound</label>
+        </div>
+        <div className={css(radioTypes)}>
+          <input type="radio" name="choose1" id="outbound" className={css(radioInput)}/>
+          <label for="outbound" className={css(radioTypeId)}>Outbound</label>
+        </div>
+      </div>
       <div className={css(inputRow)}>
         <TextInput className={css(largeInput)} placeholder="Customer/supplier BIN" />
       </div>
