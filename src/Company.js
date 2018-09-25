c54f8549a49edacfba366292bcddbe79305d8d09
Yerbol Kosbayev <yerbol_kosbayev@list.ru>
2018-09-25 16:54:58 +0600

    changed shading and colors on button hover

--- src/Company.js
+++ src/Company.js
@@ -72,7 +72,7 @@ const buttons = {
   color: '#327dd0',
   backgroundColor: '#ffffff',
   ':hover': {
-    backgroundColor: '#E1E1E1',
+    backgroundColor: '#ebf2fa',
   }
 }
 
@@ -81,6 +81,7 @@ const buttonsChecked = {
   color: '#ffffff',
   ':hover': {
     backgroundColor: '#60A3EE',
+    border: 'solid 1px #60A3EE',
   }
 }
 
