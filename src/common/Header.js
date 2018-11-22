934893a56ad50d16aa2f7b8427c2d444eceb9cf2
Timur Z <tzhkz7@gmail.com>
2018-11-22 14:24:52 +0600

    use css to set color on burger menu button

--- src/common/Header.js
+++ src/common/Header.js
@@ -39,11 +39,12 @@ const burgerButton = {
   background: 'none',
   cursor: 'pointer',
   whiteSpace: 'nowrap',
+  fill: '#FFF',
 };
 
 const burger = (
   <svg viewBox='0 0 24 24' width='24' height='24'>
-    <path fill="#FFF" d='M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z' />
+    <path d='M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z' />
   </svg>
 );
 
