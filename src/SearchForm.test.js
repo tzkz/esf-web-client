e50c428b7451b9b2635fc76178378e4611e1c16e
Timur Z <engineer@timurz.com>
2019-01-10 14:23:16 +0600

    add createQueryString() to SearchForm

--- src/SearchForm.test.js
+++ src/SearchForm.test.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
+import moment from 'moment'
 
-import 'react-dates/initialize';
+import 'react-dates/initialize'
 
-import SearchForm from './SearchForm'
+import SearchForm, { createQueryString } from './SearchForm'
+
+describe('createQueryString()', () => {
+  it('generates query string based on form object', () => {
+    const form = {
+      direction: 'INBOUND',
+      startDate: moment('2018-12-01'),
+      endDate: moment('2018-12-31'),
+    }
+
+    expect(createQueryString(form)).toEqual('?direction=INBOUND&dateFrom=2018-12-01&dateTo=2018-12-31')
+  })
+})
 
 describe('<SearchForm />', () => {
   it('renders without crashing', () => {
