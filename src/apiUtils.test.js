73e14cc5ac6a9fac7c18eb7bbbf1ea9e59e2ee53
Timur Z <engineer@timurz.com>
2019-01-11 20:42:25 +0600

    add api error parsing

--- src/apiUtils.test.js
+++ src/apiUtils.test.js
@@ -3,7 +3,13 @@ import config from './config';
 
 describe('apiUtils.js', () => {
   describe('apiCall()', () => {
-    const response = { json: jest.fn().mockResolvedValue(), ok: true }
+    const response = {
+      json: jest.fn().mockResolvedValue(),
+      ok: true,
+      headers: {
+        get: jest.fn()
+      }
+    }
     global.fetch = jest.fn().mockResolvedValue(response)
     config.apiHost = 'https://example.com'
 
