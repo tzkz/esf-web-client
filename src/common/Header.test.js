5790db0a405fdaa7e46d5f7e0bd87df696be06f4
Timur Z <engineer@timurz.com>
2019-02-22 13:22:16 +0600

    add link to title in Header

--- src/common/Header.test.js
+++ src/common/Header.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Header from './Header';
-import { css } from 'emotion';
 
 describe('<Header />', () => {
   it('should not blow', () => {
