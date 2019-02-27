babbc256609779803e40ca6155ba433498930d19
Timur Z <engineer@timurz.com>
2019-02-27 17:11:59 +0600

    add ProvideIntl to Home test

--- src/Home/Home.test.js
+++ src/Home/Home.test.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { IntlProvider } from 'react-intl';
 import { BrowserRouter as Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 
 import Component from './Home';
 
-import localeData from '../i18n/locales';
 import store from '../store';
+import ProvideIntl from '../common/ProvideIntl';
 
 const Home = (...props) => (
   <Provider store={store}>
-    <IntlProvider locale="en" messages={localeData.en}>
+    <ProvideIntl>
       <Router>
         <Component {...props} />
       </Router>
-    </IntlProvider>
+    </ProvideIntl>
   </Provider>
 )
 
