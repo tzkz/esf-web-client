5e02989b2e8ff4e7c7d60b5eadae8470ddfcdfac
Timur Z <engineer@timurz.com>
2019-02-28 18:43:54 +0600

    fix tests for SearchForm

--- src/SearchForm.test.js
+++ src/SearchForm.test.js
@@ -8,7 +8,7 @@ import SearchForm from './SearchForm'
 
 describe('<SearchForm />', () => {
   it('renders without crashing', () => {
-    const rendered = renderer.create(<SearchForm />).toJSON();
+    const rendered = renderer.create(<SearchForm onSubmit={() => {}} />).toJSON();
     expect(rendered).toBeTruthy();
   });
 
@@ -21,14 +21,16 @@ describe('<SearchForm />', () => {
     const fakeEvent = { preventDefault: jest.fn() }
 
     componentInstance.setState({
-      direction: 'OUTBOUND',
-      startDate: moment('2018-12-01'),
-      endDate: moment('2018-12-31'),
-      invoiceType: 'CREATED',
-      created: true,
-      delivered: true,
-      revoked: false,
-      cancelled: false,
+      form :{
+        direction: 'OUTBOUND',
+        startDate: moment('2018-12-01'),
+        endDate: moment('2018-12-31'),
+        invoiceType: 'CREATED',
+        created: true,
+        delivered: true,
+        revoked: false,
+        cancelled: false,
+      }
     })
     form.props.onSubmit(fakeEvent)
 
