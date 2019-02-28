5556a5c12cfa5e4a24f9d96fed195327abe6086f
Timur Z <engineer@timurz.com>
2019-02-28 16:30:55 +0600

    fix eslint errors

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,12 +4,10 @@ import Adapter from 'enzyme-adapter-react-16'
 configure({ adapter: new Adapter() })
 
 // window.matchMedia() mock for react-media
-Object.defineProperty(window, "matchMedia", {
-  value: jest.fn(() => {
-    return {
-      matches : false,
-      addListener : () => {},
-      removeListener: () => {},
-    }
-  })
+Object.defineProperty(window, 'matchMedia', {
+  value: jest.fn(() => ({
+    matches: false,
+    addListener: () => {},
+    removeListener: () => {},
+  })),
 })
