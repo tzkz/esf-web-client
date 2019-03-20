f9ef1382acf4d3492ceab1d76a9d4ebf79e4cfe4
Timur Z <engineer@timurz.com>
2019-03-20 18:51:57 +0600

    add moment locale update on locale state change

--- src/common/ProvideIntl.js
+++ src/common/ProvideIntl.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { addLocaleData, IntlProvider } from 'react-intl'
 import { connect } from 'react-redux'
+import moment from 'moment'
 
 import en from 'react-intl/locale-data/en'
 import kk from 'react-intl/locale-data/kk'
 import ru from 'react-intl/locale-data/ru'
+import 'moment/locale/kk'
+import 'moment/locale/ru'
 
 import localeData from '../i18n/locales'
 
@@ -15,17 +18,25 @@ if (!window.Intl) {
 
 addLocaleData([...en, ...kk, ...ru])
 
-const ProvideIntl = ({ children, locale }) => (
-  <IntlProvider
-    locale={locale}
-    messages={{
-      ...localeData.en,
-      ...localeData[locale],
-    }}
-  >
-    {children}
-  </IntlProvider>
-)
+moment.locale('en')
+
+const ProvideIntl = ({ children, locale }) => {
+  useEffect(() => {
+    moment.locale(locale)
+  }, [locale])
+
+  return (
+    <IntlProvider
+      locale={locale}
+      messages={{
+        ...localeData.en,
+        ...localeData[locale],
+      }}
+    >
+      {children}
+    </IntlProvider>
+  )
+}
 
 ProvideIntl.propTypes = {
   children: PropTypes.node,
